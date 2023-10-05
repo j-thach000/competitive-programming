5d59e16517b6079c2114a80a3a57a2e00e16e005
James Thach <j.thach000@gmail.com>
2023-10-05 14:55:39 -0400

    .

--- LeetCode-DSA/arrays-strings/templates/prefix_sum.py
+++ LeetCode-DSA/arrays-strings/templates/prefix_sum.py
@@ -1,4 +1,5 @@
 def prefix_sum(nums):
     prefix = [nums[0]]
     for i in range(1, len(nums)):
-        prefix.append(nums[i] + prefix[prefix.length - 1])
+        prefix.append(nums[i] + prefix[-1]) # keep adding the last prefix element to nums[i]
+                                            # the last element of prefix represents the sum up to i - 1
