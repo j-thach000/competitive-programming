b40f1956dc7518502adef7da17eae8a9b7f9424d
James Thach <j.thach000@gmail.com>
2023-10-30 16:08:22 -0400

    .

--- Codeforces/1873E.cpp
+++ Codeforces/1873E.cpp
@@ -3,37 +3,42 @@
 using namespace std;
 
 void solve() {
-    long long n, x;
-    cin >> n >> x;
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    long long low = 0;
-    long long high = 2'000'000'007;
-    while (high - low > 1) { // if the diff is > 1 then there's still possible heights to search
-        long long mid = (high + low) / 2;
-        long long total_sum = 0;
-        for (int i = 0; i < n; i++) {
-            total_sum += max(mid - a[i], 0LL);
-        }
-        if (total_sum <= x) {
-            low = mid;
-        }
-        else {
-            high = mid;
-        }
-    }
-    cout << low << endl;
+	// main idea is to binary search through all possible heights for the optimization
+	int n, x; // num of elements in array, water
+	cin >> n >> x;
+
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	long long low = 0;
+	long long high = 2000000001;
+	long long ans = 0;
+	while (high - low > 1) { // if the difference between the heights is > 1 then there's still heights to try
+		long long mid = (high + low) / 2; // height to try, does it maximize our water usage
+		long long sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += max(0LL, mid - a[i]);
+		}
+		if (sum > x) {
+			high = mid;
+		}
+		else {
+			low = mid;
+			ans = max(ans, mid);
+		}
+	}
+	cout << ans << endl;
 }
 
 int main() {
-    // magic lines to make i/o faster, refer to https://stackoverflow.com/a/31165481 if you're curious why
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	// magic lines to make i/o faster, refer to https://stackoverflow.com/a/31165481 if you're curious why
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int t; cin >> t;
-    for (int i = 0; i < t; i++) {
-        solve();
-    }
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		solve();
+	}
 }
